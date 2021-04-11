c0d6c6959f7f3480d2766b840090605c9ffc5841
wh <1069794426@qq.com>
2021-04-11 10:48:36 +0800

    .

--- plugins/request.js
+++ plugins/request.js
@@ -7,10 +7,10 @@ import axios from 'axios'
 // 创建请求对象
 export const request = axios.create({
   baseURL: 'https://conduit.productionready.io/api'
-
+})
 // 通过插件机制获取到上下文对象（query、params、req、res、app、store...）
 // 插件导出函数必须作为 default 成员
-export default({ store }) => {
+export default ({ store }) => {
 
   // 请求拦截器
   // Add a request interceptor
